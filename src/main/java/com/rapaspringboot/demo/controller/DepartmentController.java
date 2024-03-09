ef255ae93148d943af840d9c9fdf2b458f65b57d
Raj026 <rajpatani31@gmail.com>
2024-03-09 13:57:55 +0530

    Implementation of Lombok(BP Code)

--- src/main/java/com/rapaspringboot/demo/controller/DepartmentController.java
+++ src/main/java/com/rapaspringboot/demo/controller/DepartmentController.java
@@ -4,6 +4,8 @@ import com.rapaspringboot.demo.entity.Department;
 import com.rapaspringboot.demo.service.DepartmentService;
 import com.rapaspringboot.demo.service.DepartmentServiceImpl;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,11 +15,11 @@ import java.util.List;
 public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
+    private Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
     @PostMapping("/departments")
     public Department saveDepartment(@Valid @RequestBody Department department) {
+        LOGGER.info("Inside The Save Dept Logger");
         return departmentService.saveDepartment(department);
-
-
     }
 
     @GetMapping("/departments")
